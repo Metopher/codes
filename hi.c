a680e3a05ac8addd3e913357688c30383974eea5
Your Namecls <christopher.paul.alapatt@gmail.com>
2023-10-22 22:21:02 +0530

    hello guys im cubing a number

--- hi.c
+++ hi.c
@@ -2,15 +2,15 @@
 
 int number(int a)
 {
-    return a*a;
+    return a*a*a;
 }
 int main()
 {
     int a,b;
-    printf("enter a number to be squared:");
+    printf("enter a number to be cubed:");
     scanf("%d",&a);
     b=number(a);
-    printf("the sqaure of %d is %d",a,b);
+    printf("the cube of %d is %d",a,b);
     return 0;
 }
 
